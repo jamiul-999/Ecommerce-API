fd1e41ec6e45ab4a572513246ea1b6460d8f8088
charlie <jhdhj999@gmail.com>
2025-10-07 20:19:56 +0600

    Login added

--- database/user.go
+++ database/user.go
@@ -6,7 +6,7 @@ type User struct {
 	LastName     string `json:"last_name"`
 	Email        string `json:"email"`
 	Password     string `json:"password"`
-	IsShopeOwner string `json:"is_shop_owner"`
+	IsShopeOwner bool   `json:"is_shop_owner"`
 }
 
 var users []User
@@ -18,4 +18,13 @@ func (u User) Store() User {
 	u.ID = len(users) + 1
 	users = append(users, u)
 	return u
+}
+
+func Find(email, pass string) *User {
+	for _, u := range users {
+		if u.Email == email && u.Password == pass {
+			return &u
+		}
+	}
+	return nil
 }
--- rest/routes.go
+++ rest/routes.go
@@ -42,5 +42,10 @@ func initRoutes(mux *http.ServeMux, manager *middleware.Manager) {
 			http.HandlerFunc(handlers.CreateUser),
 		),
 	)
+	mux.Handle("POST /users/login",
+		manager.With(
+			http.HandlerFunc(handlers.Login),
+		),
+	)
 
 }
