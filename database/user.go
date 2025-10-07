2bfb1567419e541cf4cebeadc14cbf59271325f6
charlie <jhdhj999@gmail.com>
2025-10-07 13:45:05 +0600

    User added to database and user creation added

---
+++ database/user.go
@@ -0,0 +1,21 @@
+package database
+
+type User struct {
+	ID           int    `json:"id"`
+	FirstName    string `json:"first_name"`
+	LastName     string `json:"last_name"`
+	Email        string `json:"email"`
+	Password     string `json:"password"`
+	IsShopeOwner string `json:"is_shop_owner"`
+}
+
+var users []User
+
+func (u User) Store() User {
+	if u.ID != 0 {
+		return u
+	}
+	u.ID = len(users) + 1
+	users = append(users, u)
+	return u
+}
--- rest/routes.go
+++ rest/routes.go
@@ -7,29 +7,40 @@ import (
 )
 
 func initRoutes(mux *http.ServeMux, manager *middleware.Manager) {
-	mux.Handle("GET /products", manager.With(
-		http.HandlerFunc(handlers.GetProducts),
-	),
+	mux.Handle("GET /products",
+		manager.With(
+			http.HandlerFunc(handlers.GetProducts),
+		),
 	)
 
-	mux.Handle("POST /products", manager.With(
-		http.HandlerFunc(handlers.CreateProduct),
-	),
+	mux.Handle("POST /products",
+		manager.With(
+			http.HandlerFunc(handlers.CreateProduct),
+		),
 	)
 
-	mux.Handle("GET /products/{id}", manager.With(
-		http.HandlerFunc(handlers.GetProduct),
-	),
+	mux.Handle("GET /products/{id}",
+		manager.With(
+			http.HandlerFunc(handlers.GetProduct),
+		),
 	)
 
-	mux.Handle("PUT /products/{id}", manager.With(
-		http.HandlerFunc(handlers.UpdateProduct),
-	),
+	mux.Handle("PUT /products/{id}",
+		manager.With(
+			http.HandlerFunc(handlers.UpdateProduct),
+		),
 	)
 
-	mux.Handle("DELETE /products/{id}", manager.With(
-		http.HandlerFunc(handlers.DeleteProduct),
-	),
+	mux.Handle("DELETE /products/{id}",
+		manager.With(
+			http.HandlerFunc(handlers.DeleteProduct),
+		),
+	)
+
+	mux.Handle("POST /users",
+		manager.With(
+			http.HandlerFunc(handlers.CreateUser),
+		),
 	)
 
 }
