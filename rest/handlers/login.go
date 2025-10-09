389fda3f96a9843ff42bf21fa3c6161d6500debf
charlie <jhdhj999@gmail.com>
2025-10-09 21:32:00 +0600

    Jwt generation for login requests

--- rest/handlers/login.go
+++ rest/handlers/login.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"ecommerce-api/config"
 	"ecommerce-api/database"
 	"ecommerce-api/util"
 	"encoding/json"
@@ -29,6 +30,19 @@ func Login(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	util.SendData(w, usr, http.StatusCreated)
+	cnf := config.GetConfig()
+	accessToken, err := util.CreateJwt(cnf.JwtSecretKey, util.Payload{ //jwt secret key
+		Sub:       usr.ID,
+		FirstName: usr.FirstName,
+		LastName:  usr.LastName,
+		Email:     usr.Email,
+	})
+
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	util.SendData(w, accessToken, http.StatusCreated)
 
 }
