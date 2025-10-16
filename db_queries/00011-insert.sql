40db844e3d25de7c469473694bdea3a0d409f08d
charlie <jhdhj999@gmail.com>
2025-10-16 13:22:25 +0600

    Added basic crud sql commands as example

---
+++ db_queries/00011-insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO users (
+	first_name, 
+	last_name,
+	email,
+	password
+) VALUES (
+	'Damien',
+	'Wayne',
+	'dw@email.com',
+	'123456789'
+)
---
+++ db_queries/00013-update.sql
@@ -0,0 +1,4 @@
+UPDATE users SET
+	email = 'wayne_inc@email.com',
+	password = '786'
+WHERE id = 1;
