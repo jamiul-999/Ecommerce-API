8d6d2a2f8bbbd7e623b203d578298f8fe98a5a9d
charlie <jhdhj999@gmail.com>
2025-10-21 10:05:58 +0600

    Added domain specific directories

---
+++ domain/product.go
@@ -0,0 +1,11 @@
+package domain
+
+// model/entity
+
+type Product struct {
+	ID          int     `json:"id" db:"id"`
+	Title       string  `json:"title" db:"title"`
+	Description string  `json:"description" db:"description"`
+	Price       float64 `json:"price" db:"price"`
+	ImgURL      string  `json:"imageurl" db:"img_url"`
+}
